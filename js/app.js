9d949cc1738a7d84979e1a502dc1c84a5e602332
Ajay Kadus <ajay.kadus@gmail.com>
2014-06-28 21:12:06 -0700

    fixed bug for description not getting loaded for edit form

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 function APP() {
     this.previewProducts = [
         { id: 1, name: "PS4", description: "Game console", value: 399 },
-        { id: 2, name: "Xbox-one", description: "Game con", value: 499 },
+        { id: 2, name: "Xbox-one", description: "Game console", value: 499 },
         { id: 3, name: "Macbook Air", description: "Ultrabook", value: 999 },
         { id: 4, name: "Macbook Pro", description: "Laptop", value: 1299 },
         { id: 5, name: "Iphone 6", description: "Phone", value: 299 }
@@ -9,7 +9,7 @@ function APP() {
 
     this.MerchantProducts = [
         { id: 1, name: "PS4", description: "Game console", value: 399, width: 200, length: 300, weight: 799, height: 20},
-        { id: 2, name: "Xbox-one", description: "Game con", value: 499, width: 200, length: 300, weight: 899, height: 20},
+        { id: 2, name: "Xbox-one", description: "Game console", value: 499, width: 200, length: 300, weight: 899, height: 20},
         { id: 3, name: "Macbook Air", description: "Ultrabook", value: 999, width: 400, length: 300, weight: 2345, height: 10},
         { id: 4, name: "Macbook Pro", description: "Laptop", value: 1299, width: 500, length: 400, weight: 3456, height: 15},
         { id: 5, name: "Iphone 6", description: "Phone", value: 299, width: 20, length: 50, weight: 150, height: 5}

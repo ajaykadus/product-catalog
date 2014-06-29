2faa3da443de09def87f6f63c3ec2c50ee3db5e4
Ajay Kadus <ajay.kadus@gmail.com>
2014-06-28 19:05:49 -0700

    resolved the bug for edit product by changing to route in the browser window

--- js/views/editProduct.js
+++ js/views/editProduct.js
@@ -28,6 +28,7 @@ define([
             var updatedProduct = new Product(data);
             updatedProduct.set('id',this.model.get('id'),{silent: true});
             Backbone.Events.trigger('changed_product', updatedProduct);
+            Backbone.history.navigate('home', false);
             $(".content").show();
             $(".js-add-product").show();
             $(".js-product-details").html('');
